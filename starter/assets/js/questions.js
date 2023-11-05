32e2b821222fdefba64593beaac6ed03f7c32b65
emeritolopes <emerito.lopes@gmail.com>
2023-11-05 16:00:06 +0000

    added questions to js file

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,84 @@
+// list of questions, options and answers
+
+var questions = [
+    {
+        name: "What is a data structure?",
+        options: ["a specialized format for organizing, processing, retrieving and storing data.",
+                "a math calculation",
+                "the structure of a building", 
+                "all of the above"], 
+        answer: "a specialized format for organizing, processing, retrieving and storing data."
+    },
+    {
+        name: "Arrays in JavaScript can be used to store ...",
+        options: ["numbers and strings",
+                    "other arrays",
+                    "booleans",
+                    "all of the above"], 
+        answer: "all of the above"
+    },
+    {
+        name: "What from the follong is not a JavaScript Data Types?",
+        options: ["Numbers",
+                    "Strings",
+                    "Booleans",
+                    "Shapes"], 
+        answer: "Shapes"
+    },
+    {
+        name: "Which symbol is used for comments in JavaScript?",
+        options: ["//",
+                    "?",
+                    "!",
+                    "£"], 
+        answer: "//"
+    },
+    {
+        name: "Which company developed JavaScript?",
+        options: ["Netscape",
+                "Apple", "Microsoft", "Redhat"], 
+        answer: "Netscape"
+    },
+    {
+        name: "String values must be enclosed within ... when being assigned to variables",
+        options: ["commas",
+                 "curly brackets",
+                "quotes", 
+                "parentheses"], 
+        answer: "Quotes"
+    },
+    {
+        name: "What is the object type?",
+        options: ["compound value where you can set properties (named locations) that each hold their own values of any type",
+                    "a physical object",
+                    "hackers backpack",
+                    "all of the above"], 
+        answer: "compound value where you can set properties (named locations) that each hold their own values of any type"
+    },
+    {
+        name: "What is typeOf operator?",
+        options: ["a type of number to operate",
+                "a math calculation",
+                "operator that can examine a value and tell you what type it is",
+                "all of the above"], 
+        answer: "operator that can examine a value and tell you what type it is"
+    },
+    {
+        name: "What is scope in Javascript?",
+        options: ["a specialized format for organizing, processing, retrieving and storing data.",
+                "a math calculation",
+                "collection of variables as well as the rules for how those variables are accessed by name",
+                "all of the above"], 
+        answer: "collection of variables as well as the rules for how those variables are accessed by name"
+    },
+    {
+        name: "What is the difference between == and ===?",
+        options: ["there are not differences",
+                "== is the abstract equality operator while === is the strict equality operator.",
+                "the structure of a building",
+                "new feature in ECMAScript 5 that allows you to place a program, or a function, in a strict operating context."], 
+        answer: "== is the abstract equality operator while === is the strict equality operator.",
+    }
+
+
+]; 
