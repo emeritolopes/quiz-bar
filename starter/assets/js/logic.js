aab114121e92420397432aab7da332978a5ce7bb
emeritolopes <emerito.lopes@gmail.com>
2023-11-09 22:40:44 +0000

    added getQuestion function in js file

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,29 +1,38 @@
 // all DOM elements
 var scoreEl = document.querySelector("#score");
 var timerEl = document.querySelector("#timer");
-var startEl = document.querySelector("#start-screen");
 var startBtn = document.querySelector("#start");
 var questionsEl = document.querySelector("#questions");
 var initialsEl = document.querySelector("#initials");
 var feedbackEl = document.querySelector("#feedback");
 
 
-var currentQuestion = 0;
+var currentQuestionI = 0;
 var time = questionsEl.length * 15;
 var timerId;
 
-function startQuiz () {
 
-    //hide inital screen
-    startEl.setAttribute("class", "hide");
 
+
+function startQuiz() {
+    
+    var landingScreenEl = document.getElementById("start-screen");
+    landingScreenEl.setAttribute("class", "hide");
+    
     questionsEl.removeAttribute("class");
 
-    //start timer
-    timerId = setInterval(cloclTick, 1000);
-//show starting time
+    timerId = setInterval(clockTick, 1000);
+
     timerEl.textContent = time;
-
     getQuestion();
+   
+}
 
+function getQuestion(){
+    var currentQuestion = question[currentQuestionI];
+
+    var titleEl = document.getElementById("Question-title");
+    titleEl.textContent = currentQuestion.title;
+
+    choicesEl.innerHTML = "";
 }
